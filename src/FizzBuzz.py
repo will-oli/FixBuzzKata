7d6c5f94d8422b40592f4cd70d80e231ddef6709
Willem van Ketwich <willem.van.ketwich@accenture.com>
2022-03-04 10:04:13 +1100

    initial commit

---
+++ src/FizzBuzz.py
@@ -0,0 +1,35 @@
+
+class FizzBuzz:
+    FIZZ = "Fizz"
+    BUZZ = "Buzz"
+
+    def evaluate_number(self, number):
+        if self.__is_fizzbuzzable(number):
+            return self.FIZZ + self.BUZZ
+        elif self.__is_fuzzable(number):
+            return self.FIZZ
+        elif self.__is_buzzable(number):
+            return self.BUZZ
+        else:
+            return number
+
+    def __is_fuzzable(self, number):
+        return self.__is_multiple_of_three(number) | self.__has_number_a_three(number)
+
+    def __is_buzzable(self, number):
+        return self.__is_multiple_of_five(number) | self.__has_number_a_five(number)
+
+    def __is_fizzbuzzable(self, number):
+        return self.__is_fuzzable(number) and self.__is_buzzable(number)
+
+    def __is_multiple_of_three(self, number):
+        return number % 3 == 0
+
+    def __is_multiple_of_five(self, number):
+        return number % 5 == 0
+
+    def __has_number_a_three(self, number):
+        return 0 <= str(number).find(str(3))
+
+    def __has_number_a_five(self, number):
+        return 0 <= str(number).find(str(5))
